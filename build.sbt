11065ecd157e855a78c8ebf6964628e6a2a7d931
k-shibue <shibue@blue-corporation.jp>
2015-06-18 22:08:39 +0900

    GirlsSenseのタイムラインを取得

--- build.sbt
+++ build.sbt
@@ -5,5 +5,6 @@ version := "1.0"
 resolvers += "Atilika Open Source repository" at "http://www.atilika.org/nexus/content/repositories/atilika"
 
 libraryDependencies ++= Seq(
-  "org.atilika.kuromoji" % "kuromoji" % "0.7.7"
+  "org.atilika.kuromoji" % "kuromoji" % "0.7.7",
+  "org.twitter4j" % "twitter4j-core" % "4.0.2"
 )
--- src/main/scala/twitterResearch.scala
+++ src/main/scala/twitterResearch.scala
@@ -1,14 +1,60 @@
 import org.atilika.kuromoji.Tokenizer
 import org.atilika.kuromoji.Token
+import twitter4j._
+import twitter4j.conf._
 
-object Main extends App{
+/**
+ * Girls Senseのつぶやきを形態素解析し、どのワードを含むとリツイートされやすいか統計を取る
+ */
+object TwitterResearch {
 
-  val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
+  /**
+   * Twiiterの設定
+   */
+  def twitterInfo() = {
+    //
+  }
 
-  val tokens = tokenizer.tokenize("すもももももももものうち").toArray
+  /**
+   * Girls Senseのつぶやきを取得
+   */
+  def getTwitter(userName: String) = {
+    val cb = new ConfigurationBuilder
+    cb.setOAuthConsumerKey("wQ7lBe5gIQExkA98ocsIJa6ey")
+      .setOAuthConsumerSecret("vbRfYoPcj8PYPV8V4K6AHfQevnSfQPb8OIR9flSLxEXp3lHcBt")
+      .setOAuthAccessToken("198909960-oGoIqOLzytL3fWsTmALeLiCqL0OFHMOc5zwIV6Lo")
+      .setOAuthAccessTokenSecret("OqeqcR2kEWHXakGPD6ykDxwJj0ybtz2xUaZfixdHxUewV")
 
-  tokens.foreach { t =>
-    val token = t.asInstanceOf[Token]
-    println(s"${token.getSurfaceForm} - ${token.getAllFeatures}")
+    val twitterFactory = new TwitterFactory(cb.build)
+    val tt = twitterFactory.getInstance
+
+    val timeLine = tt.timelines
+    timeLine.getUserTimeline(userName)
+  }
+
+  /**
+   * つぶやきを形態素解析してサマる
+   */
+  def tweetAnalysis() = {
+
+    val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
+
+    val tokens = tokenizer.tokenize("すもももももももものうち").toArray
+
+    tokens.foreach { t =>
+      val token = t.asInstanceOf[Token]
+      println(s"${token.getSurfaceForm} - ${token.getAllFeatures}")
+    }
+  }
+
+  /**
+   * 実行
+   */
+  def main (args: Array[String]) {
+
+    val userName = "Girls_Sense"
+    val gsTimeLine = getTwitter(userName)
+    println(gsTimeLine)
+//    tweetAnalysis()
   }
 }
